074459bd01afe8772e4c88526d19b76382ff25eb
Josiah Maina <josiah5115@gmail.com>
2018-09-04 15:52:02 -0600

    register, login, logout

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 const checkForSession = require('./middlewares/checkForSession');
+const swag_controller = require('./controllers/swag_controller');
+const auth_controller = require('./controllers/auth_controller');
+
 
 const app = express();
 
@@ -26,9 +29,18 @@ app.use(session({
 app.use(checkForSession);
 
 
+// ENDPOINTS:
+// Swag_Controller End-Points:
+app.get('/api/swag', swag_controller.read);
+
+// Auth_Controller End-Points:
+app.post('/api/login', auth_controller.login);
+app.post('/api/register', auth_controller.register);
+app.post('/api/signout', auth_controller.signout);
+app.get('/api/user', auth_controller.getUser);
 
 
-// Server listening/ running;
+// Server listening/ running:
 app.listen(SERVER_PORT, () => {
  console.log(`The magice happens on port: ${SERVER_PORT}`)
 });
